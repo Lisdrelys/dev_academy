b520c0473a5afd419eb39887befb1aa945bf4b57
Lisdrelys <lisdrelys@gmail.com>
2021-11-02 15:37:21 +0000

    Nuevo modulo

---
+++ academy_odoo/__manifiest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{ 
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage training""",
+    
+    'description': """Academy Module to manage training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '1.0',
+    
+    'depends': ['base'],
+    
+    'data': [],
+    
+    'demo': [],   
+    
+}
