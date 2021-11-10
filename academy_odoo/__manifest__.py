643273cc8b6e33e67913e70f21b1968d09469792
Lisdrelys <lisdrelys@gmail.com>
2021-11-10 15:57:16 -0400

    Nuevo

--- academy_odoo/__manifest__.py
+++ academy_odoo/__manifest__.py
@@ -1,35 +1,38 @@
-# -*- coding: utf-8 -*-
+# _*_ coding: utf-8 -*-
 
-{ 
-    'name': 'Odoo Academy',
-    
-    'summary': """Academy app to manage training""",
-    
-    'description': """Academy Module to manage training:
+{
+
+   'name': 'Odoo Academy',
+
+   'summary': 'Aplicar descuentos en la interfaz del POS',
+
+   'version': '12.0.1.0.0',  
+
+   'description': """ Academy app.a
+        Academy modeule:
         - Courses
         - Sessions
         - Attendees
-    """,
+    """, 
+
     
-    'author': 'Odoo',
+    'author': 'Yenbust',
+
+  
+    'category': 'Traning',
     
-    'website': 'https://www.odoo.com',
-    
-    'category': 'Training',
-    
-    'version': '1.0',
+    'version': '0.1',
     
     'depends': ['base'],
-    
+
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
+        'views/session_views.xml',
     ],
     
-    'demo': [
-        'demo/academy_demo.xml',
-    ],   
-    
+    'demo': ['demo/academy_demo.xml'],    
+   
 }
--- academy_odoo/models/course.py
+++ academy_odoo/models/course.py
@@ -1,16 +1,33 @@
-# -*- coding: utf-8 -*-
+# _*_ coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     _name = 'academy.course'
-    _description = 'Course Info'
-    name = fields.Char(string='Tittle', required=True)
-    description = fields.Text(string='Description')
+    _description = 'Academy Course Model'
     
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
     level = fields.Selection(string='Level',
-                            selection=[('beginner','Beginner'), 
-                                       ('intermediate', 'Intermediate'),
-                                       ('advanced','Advanced')],
-                             copy=False)
+                            selection=[('beginner', 'Beginner'),
+                                      ('intermediate', 'Intermediate'),
+                                      ('advanced', 'Advanced')],
+                            copy=False)
     active = fields.Boolean(string='Active', default=True)
+    base_price = fields.Float(string="Base Price", default=0.00)
+    additional_fee = fields.Float(string="Additional Fee", default=0.00)
+    total_price = fields.Float(string="Total Price", readonly=True)
+    session_ids = fields.One2many(comodel_name='academy.session', inverse_name='course_id', string='Sessions')
+    
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.0:
+            raise UserError('Base price cannot be set negative.')
+        self.total_price = self.base_price + self.additional_fee
+    
+    @api.constrains('additional_fee')
+    def _check_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10.00:
+                raise ValidationError('Additonal fees cannot be less than 10.00: %s' % record.additonal_fee)
